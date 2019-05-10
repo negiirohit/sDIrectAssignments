3841b0eea2a99b80927f084f102784fd688fb4c6
Rohit Negi <rohitnegi@smartdatainc.net>
2019-05-10 09:57:45 +0530

    before image compression

---
+++ jqueryApp/jqueryApp@/controllers/chatController.js
@@ -0,0 +1,51 @@
+const Chat = require('../models/chat');
+const User = require('../models/user');
+
+module.exports.getChatMessgaes = (req, res, next ) => {
+    //console.log(req.user.id);
+    Chat.findOne({ chatRoom : req.params.chatRoom })
+    .then((chat) => {
+            //console.log("chat messages: "+chat.messages);
+            res.status(200);                
+            res.json({
+                success: true,
+                message: 'Fetched all messages ',
+                data : {messages : chat.messages}
+            });
+    })
+    .catch((err) => {
+        res.status(500);        
+        res.json({
+            success : false,
+            message : err.message,
+        }); 
+    });
+}
+
+
+
+//chat controller for save files in database
+module.exports.saveMsg = (msg) => {
+    console.log("msg in controller: "+JSON.stringify(msg));
+     Chat.findOneAndUpdate({chatRoom : msg.room},{$push:{messages: msg} },{new:true})
+     .then(chat => {
+            console.log("msg saved succesfully");
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+
+module.exports.changeMsgStatus = (msg) => {
+    console.log("msg id"+msg.msg_id);
+    Chat.updateOne({chatRoom : msg.room,"messages.msg_id":msg.msg_id },
+         { $set: { "messages.$.status" : msg.status } })
+            .then(res => {
+             console.log("msg updated"+JSON.stringify(res));
+        })
+         .catch(err=>{
+         console.log(err);
+     } )
+    
+}
---
+++ jqueryApp/jqueryApp@/controllers/fileController.js
@@ -0,0 +1,7 @@
+const Chat = require('../models/chat');
+const User = require('../models/user');
+
+module.exports.uploadImages = (req, res, next ) => {
+    
+
+}
